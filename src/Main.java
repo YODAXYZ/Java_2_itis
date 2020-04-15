cbded0ef49c9cbea461289a8b6e7d43d4ab59dcb
Александр <kas1yoda@gmail.com>
2020-04-15 13:01:27 +0300

    hw14 YAML JSON

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import Map.HashMap;
+import org.json.JSONObject;
 
 import java.util.Map;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
---
+++ src/Path/ClassWork.java
@@ -0,0 +1,8 @@
+package Path;
+
+public class ClassWork {
+    public static void main(String[] args) {
+        System.out.println(ClassWork.class.getResource("."));
+
+    }
+}
